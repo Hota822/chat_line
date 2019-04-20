f11289aec171b0060150fd1bd35a4484d3c51ad4
hota822 <hota822@gmail.com>
2019-04-21 00:02:39 +0900

    create partial(header, shim, rails)

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
 
   root 'static_pages#home'
+  get '/signup', to: 'users#new'
+  get '/login', to: 'sessions#new'
+  post '/login', to: 'sesisons#create'
+  delete '/login', to: 'sessions#destroy'
+  resources :users
 end
